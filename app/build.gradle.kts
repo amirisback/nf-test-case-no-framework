e19cb64e890e988c30b68cd09d91023a0ef4f0ba
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-01-28 23:17:15 +0700

    update: README.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,7 @@ android {
         // Declaration apps name debug mode
         val debugAttribute = "Development"
         val nameAppDebug = "${ProjectSetting.NAME_APP} $debugAttribute"
-
-        resConfigs("en", "id")
+        resourceConfigurations += setOf("en", "id")
 
         // Inject app name for debug
         resValue("string", "app_name", nameAppDebug)
@@ -61,9 +60,13 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-
     packagingOptions {
-        exclude("META-INF/*")
+        jniLibs {
+            excludes += setOf("META-INF/*")
+        }
+        resources {
+            excludes += setOf("META-INF/*")
+        }
     }
 
 }
